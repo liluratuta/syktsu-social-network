ad86d0efa055a4b1b0fd6b6e1da39b22b11a3a54
mishvtka <aerovain@gmail.com>
2019-04-02 11:12:43 +0300

    update

--- layout-misha/index.js
+++ layout-misha/index.js
@@ -2,3 +2,6 @@ document.getElementById('reg-display-button').onclick = function(){
 	document.getElementById('auth').style.display = 'none';
 	document.getElementById('registration').style.display = 'block';
 }
+function setting_show(){
+	document.getElementById('set-div').style.visibility = "visible";
+}
